a3b44d2235128c4929545bc0aa97fd6a6f7c5c7b
Ramie Bashir <ramiebash@gmail.com>
2022-09-12 19:43:56 +0300

    work in progress

---
+++ src/images.js
@@ -0,0 +1,10 @@
+import img1 from './images/Viking-helmet.jpg';
+import img2 from './images/Aladdin_Disney_pose.png';
+import img3 from './images/cowboy-boots-hat.jpg';
+import img4 from './images/LetterA.svg.png';
+import img5 from './images/apple.jpg';
+import img6 from './images/Banana-Single.jpg';
+import img7 from './images/1200px-LetterB.svg.png';
+import img8 from './images/Capital_C.svg.png';
+
+export default [img1, img2, img3, img4, img5, img6, img7, img8];
