8d626bdab9f369bfb632b8b65b97dd7341464cf0
“Maysan <maysanir@mta.ac.il>
2022-09-14 20:38:38 +0300

    update css

--- src/App.jsx
+++ src/App.jsx
@@ -1,106 +1,123 @@
-import React from 'react';
-import Images from './images';
-import {useState, useEffect} from "react";
-import {shuffle} from 'lodash';
+import React from "react";
+import Images from "./images";
+import { useState, useEffect } from "react";
+import { shuffle } from "lodash";
+import statsBackground from "./images/statsBackground.png";
 
 function App() {
-    const [cards,setCards] = useState( shuffle([...Images, ...Images]) );
-    const [clicks,setClicks] = useState(0);
-    const [won,setWon] = useState(false);
-    const [activeCards,setActiveCards] = useState([]);
-    const [foundPairs,setFoundPairs] = useState([]);
-    const [count, setCount] = useState(100);
+  const [cards, setCards] = useState(shuffle([...Images, ...Images]));
+  const [clicks, setClicks] = useState(0);
+  const [won, setWon] = useState(false);
+  const [activeCards, setActiveCards] = useState([]);
+  const [foundPairs, setFoundPairs] = useState([]);
+  const [count, setCount] = useState(60);
 
-    useEffect(() => {
-      if (count > 0) {
+  useEffect(() => {
+    if (count > 0) {
       setInterval(() => {
         // setCount(prevCount => (prevCount > 0 ? prevCount - 20 : prevCount));
-        setCount(prevCount => prevCount - 20);
-      }, 1000);}
-      else {
-        setCount(0);
-      }
-    }, []);
-
-    if (count==0) {
-      alert("GAME OVER!")
+        setCount((prevCount) => prevCount - 1);
+      }, 1000);
+    } else {
+      setCount(0);
     }
+  }, []);
 
-    function startOver() {
+  if (count == 0) {
+    alert("GAME OVER!");
+  }
+
+  function startOver() {
+    setCards(shuffle([...Images, ...Images]));
+    setFoundPairs([]);
+    setWon(false);
+    setClicks(0);
+    setCount(60);
+    setActiveCards([]);
+  }
+
+  function flipCard(index) {
+    if (won) {
       setCards(shuffle([...Images, ...Images]));
-        setFoundPairs([]);
-        setWon(false);
-        setClicks(0);
-        setCount(100);
-        setActiveCards([]);
+      setFoundPairs([]);
+      setWon(false);
+      setClicks(0);
+      setCount(60);
     }
 
-    function flipCard(index) {
-      if (won) {
-        setCards(shuffle([...Images, ...Images]));
-        setFoundPairs([]);
-        setWon(false);
-        setClicks(0);
-        setCount(100);
-      }
-    
-      if (activeCards.length === 0) {
-        setActiveCards([index]);
-      }
-      if (activeCards.length === 1) {
-        const firstIndex = activeCards[0];
-        const secondsIndex = index;
-        if (cards[firstIndex] === cards[secondsIndex]) {
-          if (foundPairs.length + 2 === cards.length) {
-            setWon(true);
-          }
-          setFoundPairs( [...foundPairs, firstIndex, secondsIndex] );
-        }
-        setActiveCards([...activeCards, index]);
-      }
-      if (activeCards.length === 2) {
-        setActiveCards([index]);
-      }
-      if(!won) {
-        setClicks(clicks + 1);
-      }
+    if (activeCards.length === 0) {
+      setActiveCards([index]);
     }
-    return (
-      <div>
-        <div className="board">
-          {cards.map((card,index) => {
-            const flippedToFront =  (activeCards.indexOf(index) !== -1) || foundPairs.indexOf(index) !== -1;
-            return (
-              <div className={"card-outer " + (flippedToFront ? 'flipped' : '')}
-                   onClick={() => flipCard(index)}>
-                <div className="card">
-                  <div className="front">
-                    <img src={card} alt=""/>
-                  </div>
-                  <div className="back" />
-                </div>
-              </div>
-            );
-          })}
-        </div>
+    if (activeCards.length === 1) {
+      const firstIndex = activeCards[0];
+      const secondsIndex = index;
+      if (cards[firstIndex] === cards[secondsIndex]) {
+        if (foundPairs.length + 2 === cards.length) {
+          setWon(true);
+        }
+        setFoundPairs([...foundPairs, firstIndex, secondsIndex]);
+      }
+      setActiveCards([...activeCards, index]);
+    }
+    if (activeCards.length === 2) {
+      setActiveCards([index]);
+    }
+    if (!won) {
+      setClicks(clicks + 1);
+    }
+  }
+  return (
+    <div className="game">
+      <div className="statsPane">
         <div className="stats">
           {won && (
-            <>You won the game! Congratulations!<br />
-              Click any card to play again.<br /><br />
+            <>
+              You won the game! Congratulations!
+              <br />
+              Click any card to play again.
+              <br />
+              <br />
             </>
           )}
-          Clicks: {clicks} &nbsp;&nbsp;&nbsp;
-          Found pairs:{foundPairs.length/2}
+          Clicks: {clicks}
+          <br />
+          <br />
+          Found pairs:
+          {foundPairs.length / 2}
+          <br />
           <br />
           Timer: {count}
           <br />
-          <button onClick={startOver}>
-          Start Over
-          </button>
-
+          <div className="button">
+            <button onClick={startOver}>Start Over</button>
+          </div>
+        </div>
+        <div className="statsBackground">
+          <img src={statsBackground} className="statsBackroundImg" alt="..." />
         </div>
       </div>
-    );
-  }
-  
-  export default App;
+      <div className="board">
+        {cards.map((card, index) => {
+          const flippedToFront =
+            activeCards.indexOf(index) !== -1 ||
+            foundPairs.indexOf(index) !== -1;
+          return (
+            <div
+              className={"card-outer " + (flippedToFront ? "flipped" : "")}
+              onClick={() => flipCard(index)}
+            >
+              <div className="card">
+                <div className="front">
+                  <img src={card} alt="" />
+                </div>
+                <div className="back" />
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
